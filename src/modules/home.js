7483e718df0491748c34d563caa6d7dd3d5e1761
Yasin Aydas <115567753+iysz18@users.noreply.github.com>
2025-03-07 15:08:28 +0100

    Function renderHome generates now the home tab.

---
+++ src/modules/home.js
@@ -0,0 +1,8 @@
+import "../styles/home.css";
+
+export default function renderHome() {
+    const contentWrapper = document.createElement("div");
+    contentWrapper.classList.add("content-wrapper");
+
+    return contentWrapper;
+}
