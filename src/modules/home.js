00d1c845cd8d712ff7060af3e3aa802e4e01153f
Yasin Aydas <115567753+iysz18@users.noreply.github.com>
2025-03-07 17:21:14 +0100

    Added the needed hero and substr of the hero to the renderHome function.

--- src/modules/home.js
+++ src/modules/home.js
@@ -1,8 +1,21 @@
 import "../styles/home.css";
 
 export default function renderHome() {
+    // contentWrapper will contain the necessary content like h1 p for substring
     const contentWrapper = document.createElement("div");
     contentWrapper.classList.add("content-wrapper");
+
+    // create h1 for hero string
+    const heroStr = document.createElement("h1");
+    heroStr.textContent = "Welcome to Ember Steakhouse – Where Fire Meets Flavor";
+    heroStr.classList.add("heroStr");
+    contentWrapper.appendChild(heroStr);
+
+    // p element for substring beneath the hero string
+    const substr = document.createElement("p");
+    substr.textContent = "Experience the Sizzle at Ember Steakhouse – Where Fire and Flavor Ignite";
+    substr.classList.add("subStr")
+    contentWrapper.appendChild(substr);
 
     return contentWrapper;
 }
